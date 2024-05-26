0524395487f57664ecd6b19f7b30c3330c050cb4
polozoff.stephen <polozoff.stephen@heb.com>
2024-05-26 08:25:01 -0700

    add create user tests and make User Dto flexible

--- src/main/java/com/sp1222/language/daybyday/api/dto/UserDto.java
+++ src/main/java/com/sp1222/language/daybyday/api/dto/UserDto.java
@@ -1,5 +1,12 @@
 package com.sp1222.language.daybyday.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
     public Long id;
     public String username;
--- src/test/groovy/com/sp1222/language/daybyday/api/service/UserServiceSpec.groovy
+++ src/test/groovy/com/sp1222/language/daybyday/api/service/UserServiceSpec.groovy
@@ -1,4 +1,91 @@
 package com.sp1222.language.daybyday.api.service
 
-class UserServiceSpec {
+import com.sp1222.language.daybyday.api.configurations.PasswordEncoderConfig
+import com.sp1222.language.daybyday.api.dto.UserDto
+import com.sp1222.language.daybyday.api.entities.user.UserInsensitive
+import com.sp1222.language.daybyday.api.entities.user.UserSensitive
+import com.sp1222.language.daybyday.api.exceptions.DuplicatePropertyException
+import com.sp1222.language.daybyday.api.repositories.UserConfidentialRepository
+import com.sp1222.language.daybyday.api.repositories.UserInsensitiveRepository
+import com.sp1222.language.daybyday.api.repositories.UserSensitiveRepository
+import spock.lang.Specification
+
+class UserServiceSpec extends Specification {
+
+    UserInsensitiveRepository userInsensitiveRepository
+    UserSensitiveRepository userSensitiveRepository
+    UserConfidentialRepository userConfidentialRepository
+    PasswordEncoderConfig passwordEncoderConfig
+    UserService userService
+
+    def setup() {
+
+        userInsensitiveRepository = Mock(UserInsensitiveRepository)
+        userSensitiveRepository = Mock(UserSensitiveRepository)
+        userConfidentialRepository = Mock(UserConfidentialRepository)
+        passwordEncoderConfig = Mock(PasswordEncoderConfig)
+        userService = new UserService(userInsensitiveRepository, userSensitiveRepository, userConfidentialRepository, passwordEncoderConfig)
+    }
+
+    def 'Create user happy path'() {
+        setup:
+        def id = 1
+        def username = "username"
+        def firstname = "firstname"
+        def email = "test@test.com"
+        userInsensitiveRepository.existsByUsername(username) >> false
+        userSensitiveRepository.existsByEmail(email) >> false
+        userInsensitiveRepository.save(_ as UserInsensitive) >> new UserInsensitive(id: id, firstname: firstname, username: username)
+        userSensitiveRepository.save(_ as UserSensitive) >> new UserSensitive(id: id, email: email, username: username)
+
+        when:
+        def userDto = UserDto.builder()
+            .firstname("firstname")
+            .email("test@test.com")
+            .username("username")
+            .build()
+        def result = userService.createUser(userDto)
+
+        then:
+        result.id == id
+        result.firstname == firstname
+        result.email == email
+        result.username == username
+    }
+
+    def 'Create user invalid username'() {
+        setup:
+        def username = "username"
+        userInsensitiveRepository.existsByUsername(username) >> true
+
+        when:
+        def userDto = UserDto.builder()
+            .firstname("firstname")
+            .email("test@test.com")
+            .username("username")
+            .build()
+        userService.createUser(userDto)
+
+        then:
+        thrown(DuplicatePropertyException)
+    }
+
+    def 'Create user invalid email'() {
+        setup:
+        def username = "username"
+        def email = "test@test.com"
+        userInsensitiveRepository.existsByUsername(username) >> false
+        userSensitiveRepository.existsByEmail(email) >> true
+
+        when:
+        def userDto = UserDto.builder()
+            .firstname("firstname")
+            .email("test@test.com")
+            .username("username")
+            .build()
+        userService.createUser(userDto)
+
+        then:
+        thrown(DuplicatePropertyException)
+    }
 }
